c3ccd6afbb92b53f16d8085bd5960e7a1aa32c23
SaharHussaini1999 <s_hussaini@kbtu.kz>
2025-06-03 18:11:27 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,364 @@
+import sys
+print(sys.version)
+
+
+print("Hello, World")
+
+
+if 5 > 2:
+  print("Five is greather than two")
+
+
+if 5 > 2:
+   print("Five is greather than two")
+if 5 > 2:
+      print("Five is greather than two")
+
+
+
+if 4 > 3:
+    print("this two number is  the same") 
+      print("Four is not bigger than Five")
+
+
+x = 5
+y = "Ali"
+print(x)
+print(y)
+
+
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = b"Hello"
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = bytearray(5)
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+
+
+#this is a comment
+print("Hello,World!")
+
+
+#this is a comment
+print("All in one!")
+
+
+print("All in one!") #tis is a comment.
+
+
+#print("Hello, World!)
+print("Cheers, Mate!")
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+x = 4 
+x = "Sally"
+print(x)
+
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x)
+print(y)
+print(z)
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+
+
+a = 4
+A = "Sally"
+print(a)
+print(A)
+
+
+x = "Hello World"
+
+#disply x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 20
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)
+
+
+x = 20.5
+
+#disply x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 1j
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = ("apple", "banana", "cherry")
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = range(6)
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = {"apple", "banana", "cherry"}
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+print(type(x))
+
+
+x = None
+
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+import random
+
+print(random.randrange(1, 10)
+
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+
+#You can use double or single quotes:
+
+print("Hello")
+print('Hello')
+
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+a = "Hello"
+print(a)
+
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
+
+for x in "banana":
+  print(x)
+
+
+a = "Hello, World!"
+print(len(a)
+
+
+txt = "The best things in life are free!"
+print("free" in txt
+
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
+
+
+
